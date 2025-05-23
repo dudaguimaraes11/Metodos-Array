9625b0b54ed82fd61bdac34ef3cd7a6bb235338a
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-05-23 09:58:35 -0300

    Adiciona o arquivo Slice-exemplo à pasta Método Array

--- Join-exemplo.js
+++
@@ -1,2 +0,0 @@
-let playlist = ["Funk", "Hip-Hop", "Pop", "Trap"];
-console.log (`Playlist como String: ${playlist.join (`,`)}.`)
---
+++ Slice-exemplo.js
@@ -0,0 +1,5 @@
+// índice inicial (inclusivo), índice final (exclusivo)
+let playlist = ["Funk", "Hipp-Hop", "Pop", "Rock", "Jazz", "Reggae", "Classical", "Indie"];
+
+console.log (`Parte da playlist (índices 2 a 5): ${playlist.slice(2,6)}).`);
+console.log (`Parte da playlist (índices 0 a 3): ${playlist.slice(0,4)}).`);
