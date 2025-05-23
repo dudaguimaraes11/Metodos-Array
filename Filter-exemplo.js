0f02a5c4830c4c202112f5eef67a3034201e5655
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-05-23 11:29:08 -0300

    Adiciona o arquivo Filter-exemplo.js

---
+++ Filter-exemplo.js
@@ -0,0 +1,6 @@
+// Cria um novo array com elementos que passam em uma condição
+
+let playlist = ["Funk", "Hip-Hop", "Pop", "Trap"];
+let playlistFiltrada = playlist.filter(musica => musica.length > 4); 
+console.log (`Playlist filtrada (músicas com mais de 4 letras): ${playlistFiltrada}`);
+
