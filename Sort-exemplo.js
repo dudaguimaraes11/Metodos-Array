5407a00b51fbb3ac0fb61b6c5d9e8a939847ebc4
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-05-23 11:49:58 -0300

    Modifica o arquivo Sort-exemplo.js

--- Sort-exemplo.js
+++ Sort-exemplo.js
@@ -1,5 +1,20 @@
 // Ordena os elementos do array
 
-let playlist = ["Funk", "Hip-Hop", "Pop", "Trap"];
+let playlist = ["Funk", "Hip-Hop", "Pop", "Indie"];
 let playlistOrdenada = playlist.sort(); 
 console.log (`Playlist ordenada: ${playlistOrdenada}.`);
+
+// Ordenar números 
+
+let numeros = [10, 5, 100, 50, 1]; 
+
+console.log (`Antes do sort: ${numeros}`); 
+
+// Ordenando números de forma crescente
+
+let numerosOrdenadosCrescentes = numeros.sort ((a,b) => a - b);
+console.log (`Números em ordem crescente: ${numerosOrdenadosCrescentes}.`); 
+
+// Ordenando os números de forma decrescente 
+let numerosOrdenadosDecrescentes = numeros.sort ((a, b) => b - a); 
+console.log (`Números em ordem decrescente: ${numerosOrdenadosDecrescentes}.`); 
