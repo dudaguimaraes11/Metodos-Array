22aaf63c250ce7e7d3e3353c6f2fefb5879013d8
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-05-23 10:02:51 -0300

    Modifica o arquivo Slice-exemplo.js

--- Slice-exemplo.js
+++ Slice-exemplo.js
@@ -3,3 +3,4 @@ let playlist = ["Funk", "Hipp-Hop", "Pop", "Rock", "Jazz", "Reggae", "Classical"
 
 console.log (`Parte da playlist (índices 2 a 5): ${playlist.slice(2,6)}).`);
 console.log (`Parte da playlist (índices 0 a 3): ${playlist.slice(0,4)}).`);
+console.log (`Parte da Playlist (índice 4): ${playlist.slice(3,4)}.`);
