b9ab0f3d0e24c73f01430d072dc2309d0078690e
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-05-23 11:42:40 -0300

    Adiciona o arquivo Sort-exemplo.js

---
+++ Sort-exemplo.js
@@ -0,0 +1,5 @@
+// Ordena os elementos do array
+
+let playlist = ["Funk", "Hip-Hop", "Pop", "Trap"];
+let playlistOrdenada = playlist.sort(); 
+console.log (`Playlist ordenada: ${playlistOrdenada}.`);
