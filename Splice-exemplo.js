9a6d257b52afa4f6434d8a6d0a619f595c691dac
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-05-23 10:45:08 -0300

    Modifica o arquivo Slice-exemplo.js

--- Splice-exemplo.js
+++ Splice-exemplo.js
@@ -1,9 +1,16 @@
-let playlist = ["Funk", "Hip-Hop", "Pop", "Rock", "Jazz"]; 
+let playlist = ["Funk", "Hip-Hop", "Pop", "Rock", "Jazz", "Hino"]; 
 
 console.log (`Antes do splice: ${playlist}.`); 
 
 // Removendo 2 elementos a partir do índice 2 
-let elementosRemovidos = playlist.splice(2,2); 
+//let elementosRemovidos = playlist.splice(2,2); 
 
-console.log (`Depois o splice: ${playlist}.`); 
-console.log (`Elementos removidos: ${elementosRemovidos}.`)
+//console.log (`Depois do splice: ${playlist}.`); 
+//console.log (`Elementos removidos: ${elementosRemovidos}.`);
+
+// Remover 2 elementos após o índice 3 
+let desafio =  playlist.splice (3, 2); 
+
+console.log (`Elementos removidos após o índice 3: ${desafio}.`); 
+console.log (`Playlist após remover os dois elementos após o índice 3: ${playlist}`);
+
