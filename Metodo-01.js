166a024cf2ddac76027b909f5b4e89fad194560b
Maria Eduarda Ferreira Guimarães <maria.e.guimaraes19@aluno.senai.br>
2025-05-23 08:59:18 -0300

    Adiciona Método Shift e Unshifit no arquivo Metodo-01

--- Metodo-01.js
+++ Metodo-01.js
@@ -23,3 +23,15 @@ let segundaMusicaRemovida = playlist.pop();
 console.log (`Após a segunda música removida: ${playlist}.`)
 console.log (`A segunda música removida: ${segundaMusicaRemovida}.`)
 
+// Removendo a primeira músic da playlist
+let musicaRemovida = playlist.shift(); 
+
+console.log (`Depois do shift: ${playlist}.`);
+console.log (`Música Removida: ${musicaRemovida}.`);
+
+// Adicionando uma nova música no ínicio da Playlist
+console.log (`Antes do Unshift: ${playlist}.`); 
+
+playlist.unshift (`Rock`); 
+
+console.log (`Depois do Unshift: ${playlist}.`);
